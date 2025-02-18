620813494a06109397a1d76a7682da5e0471e7e8
icondor2019 <ivalrxis14@gmail.com>
2025-02-17 21:07:29 -0500

    <ADD: bot valid responses challenge 8>

---
+++ app/constants/damage_constants.py
@@ -0,0 +1,15 @@
+altscore_result = {
+    "first_check_complete": True,
+    "second_check_complete": True,
+    "third_check_complete": True
+}
+
+exposed_api = 'https://3bf1-179-49-14-229.ngrok-free.app'
+
+damage_constant = {
+    "navigation": "NAV-01",
+    "communications": "COM-02",
+    "life_support": "LIFE-03",
+    "engines": "ENG-04",
+    "deflector_shield": "SHLD-05"
+}
